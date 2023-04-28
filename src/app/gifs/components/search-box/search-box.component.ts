38e3d9edece20340c97126f9301b9f84b3bb0b02
LuisMartinezJoseDaniel <josedan.jd94@gmail.com>
2023-04-28 17:55:46 -0600

    Primer commit

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,21 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrls: ['./search-box.component.css'],
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput') // Nombre del componente
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService) {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+  }
+}
